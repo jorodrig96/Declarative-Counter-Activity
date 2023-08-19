527fa99b7d8f8f9a690497aa411ce2b150e36a8d
Jose Rodriguez <joserodriguez@Joses-MBP-2.home>
2023-08-19 12:29:14 -0700

    updates automatically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React from 'react';
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [ counter, changeCounter ] = useState(1) 
+{/* you can set useState to whatever you'd like. Its what will be passed into "counter" which is the first item in the array. */}
+    window.changeCounter = changeCounter
+{/* "window." is what allows for the page to update automatically. 
+Calling the function, changeCounter, inside of the developer tools of course. 
+So if you changeCounter(whatever argument is passed in here) in developer tools it'll update the page 
+automatically.*/}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>{counter}</h1>
+    <h4>{counter}</h4>
+{/*{counter} is the javascript being interprolated into jsx using the curly brackets.*/} 
     </div>
   );
 }
